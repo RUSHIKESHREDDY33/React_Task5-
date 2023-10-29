07d1fcf1811834b4946a29c77ad01cdb78b0b350
Pushpak1712 <86339846+Pushpak1712@users.noreply.github.com>
2023-10-29 13:56:59 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section>
       <h2>About Me</h2>
       <p>
-        "Hello, my name is Rushikesh Reddy. I am a passionate web designer and a seasoned developer proficient in both front-end and back-end development. With a keen interest in creating visually captivating and functional websites, I have acquired a comprehensive understanding of various programming languages and tools essential for crafting dynamic and user-friendly online experiences. My dedication to staying updated with the latest trends and advancements in the industry allows me to deliver high-quality, scalable, and efficient solutions for any web development project."
+        "Hello, my name is Pushpak Thokala. I am a passionate web designer and a seasoned developer proficient in both front-end and back-end development. With a keen interest in creating visually captivating and functional websites, I have acquired a comprehensive understanding of various programming languages and tools essential for crafting dynamic and user-friendly online experiences. My dedication to staying updated with the latest trends and advancements in the industry allows me to deliver high-quality, scalable, and efficient solutions for any web development project."
       
       </p>
       
